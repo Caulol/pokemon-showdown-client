c0538200e6b0b63a100f8115b970790d19d080f5
Caulol <cauavitormbs@gmail.com>
2025-07-05 21:50:47 -0300

    Update config.js

--- play.pokemonshowdown.com/config/config.js
+++ play.pokemonshowdown.com/config/config.js
@@ -1,1 +1,34 @@
-../../config/config.js
+var Config = Config || {};
+
+/* version */ Config.version = "0";
+
+Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
+
+Config.whitelist = [
+	'wikipedia.org',
+
+	// The full list is maintained outside of this repository so changes to it
+	// don't clutter the commit log. Feel free to copy our list for your own
+	// purposes; it's here: https://play.pokemonshowdown.com/config/config.js
+
+	// If you would like to change our list, simply message Zarel on Smogon or
+	// Discord.
+];
+
+// `defaultserver` specifies the server to use when the domain name in the
+// address bar is `Config.routes.client`.
+Config.defaultserver = {
+	id: 'storyshowdown',
+	host: 'sim3.psim.us',
+	port: 443,
+	httpport: 6996,
+	altport: 80,
+	registered: true
+};
+
+Config.roomsFirstOpenScript = function () {
+};
+
+Config.customcolors = {
+	'zarel': 'aeo'
+};
